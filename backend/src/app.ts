691dd3561d54b0606ba894c1842d6d789f507e00
Trejon <trejonstallsworth@gmail.com>
2023-03-29 16:53:56 -0600

    started adding routing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,15 @@
-import express, {Response, Request} from 'express'
+import express, { Response, Request } from 'express'
+import { router } from './utils'
 
 export const app = express()
-let port = 3000;
+const port = 3000;
 
 
-app.get('/', (req: Request, res: Response, next: () => void) => {
-  res.send("Hello World");
-})
+// app.get('/', (req: Request, res: Response, _next: () => void) => {
+//   res.send("Hello World");
+// })
+
+app.use(router)
 
 app.listen(port, () => {
   console.log(`I am now listening on port http://localhost:${port}`);
